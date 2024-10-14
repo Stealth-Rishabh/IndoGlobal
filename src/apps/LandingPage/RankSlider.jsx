96ee044170151a3c2978b1f5195bc45bb8760f30
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-10-14 13:45:29 +0530

    Made Rankslideer component responsive

--- src/apps/LandingPage/RankSlider.jsx
+++ src/apps/LandingPage/RankSlider.jsx
@@ -51,52 +51,52 @@ const RankSlider = () => {
               <CarouselItem key={index}>
                 <div className="p-1">
                   <Card className="bg-transparent border-none rounded-none shadow-none">
-                    <CardContent className="p-0  h-96 sm:h-[400px] border-none shadow-none grid ">
-                      <div className="flex justify-between w-full">
-                        <h2 className="text-4xl font-semibold tracking-tight scroll-m-20 first:mt-0">
+                    <CardContent className="p-0  h-[640px] sm:h-[400px] border-none shadow-none grid ">
+                      <div className="flex md:flex-row flex-col sm:justify-between w-full gap-5 sm:gap-0">
+                        <h2 className=" text-2xl  sm:text-4xl font-semibold tracking-tight scroll-m-20 first:mt-0">
                           Rankings, Accereditations <br /> &amp; Tie-up.
                         </h2>
                         <ButtonSq
                           label="Know more..."
-                          iconDiv="bg-primary-color h-14 flex item-center justify-center"
-                          className="text-white h-14 bg-secondary-color hover:bg-secondary-color/90"
-                          iconStyle="mt-3"
+                          iconDiv="bg-primary-color sm:h-14 flex item-center justify-center"
+                          className="text-white text-xs  sm:h-14 bg-secondary-color hover:bg-secondary-color/90 w-fit"
+                          iconStyle="h sm:mt-3"
                         />
                       </div>
-                      <div className="flex justify-between ">
+                      <div className="flex sm:flex-row flex-col-reverse gap-4 sm:gap-0 mt- sm:mt-0 item-start sm:justify-between ">
                         <div className="grid content-end justify-end grid-cols-3 gap-3 pb-10">
-                          <div className="flex flex-col items-center content-end justify-end">
-                            <h2 className="flex items-center text-4xl font-bold tracking-tight scroll-m-20 first:mt-0 text-secondary-color">
+                          <div className="flex flex-col items-center sm:content-end sm:justify-end">
+                            <h2 className="flex items-center sm:text-4xl font-bold tracking-tight scroll-m-20 first:mt-0 text-secondary-color">
                               Ranked
                             </h2>
-                            <span className="text-[10rem] my-0 pt-0 font-extrabold stroke-text leading-none">
+                            <span className="text-[7rem] sm:text-[10rem] my-0 pt-0 font-medium sm:font-extrabold stroke-text leading-none">
                               #
                             </span>
                           </div>
 
-                          <span className="text-[15rem] font-extrabold stroke-text flex leading-none p-0 ">
+                          <span className="text-[9rem] sm:text-[15rem] font-semibold sm:font-extrabold stroke-text flex leading-none p-0 ">
                             3
                           </span>
-                          <div className="flex flex-col justify-end gap-2 pb-5 leading-none text-left">
-                            <p className="text-4xl font-bold text-secondary-color">
+                          <div className="flex flex-col justify-end sm:gap-2 pb-5 leading-none text-left">
+                            <p className="text-2xl sm:text-4xl font-bold text-secondary-color">
                               Among
                             </p>
-                            <p className="text-4xl font-bold text-secondary-color">
+                            <p className="text-2xl sm:text-4xl font-bold text-secondary-color">
                               Group of
                             </p>
-                            <p className="text-4xl font-bold text-red-500">
+                            <p className="text-2xl sm:text-4xl font-bold text-red-500">
                               Colleges
                             </p>
-                            <p className="text-4xl font-bold text-red-500">
+                            <p className="text-2xl sm:text-4xl font-bold text-red-500">
                               by PTU
                             </p>
                           </div>
                         </div>
-                        <div className="grid content-end justify-end">
+                        <div className="grid sm:content-end sm:justify-end">
                           <img
                             src={img}
                             alt="Slider imgs "
-                            className="object-contain h-[90%] m"
+                            className="object-contain h-48 sm:h-[90%] mx-auto"
                           />
                         </div>
                       </div>
@@ -107,12 +107,14 @@ const RankSlider = () => {
             ))}
           </CarouselContent>
           <CarouselPrevious
-            iconStyle="text-white lg:h-8  "
-            className="absolute left-[0px] top-[110%] bg-gray-300 hover:bg-red-600 h-10 active:bg-red-700 w-24 rounded-sm opacity-100 "
+           
+            iconStyle="text-white h-4 lg:h-8"
+            className="absolute sm:left-[0px] left-[20%] top-[96.5%] sm:top-[110%] bg-gray-300 hover:bg-red-600 h-8  w-20 active:bg-red-700 sm:w-24 rounded-none opacity-100"
           />
           <CarouselNext
-            iconStyle="text-white lg:h-8"
-            className="absolute left-[110px] top-[110%] bg-slate-300 hover:bg-red-600 w-24 h-10 rounded-sm opacity-100 active:bg-red-700 "
+            
+            iconStyle="text-white h-4 lg:h-8"
+            className="absolute sm:left-[110px] left-[55%] top-[96.5%] sm:top-[110%] bg-slate-300 hover:bg-red-600 w-20 sm:w-24 h-8  rounded-none opacity-100 active:bg-red-700"
           />
         </Carousel>
         <div className="flex justify-center mt-4 space-x-2">
