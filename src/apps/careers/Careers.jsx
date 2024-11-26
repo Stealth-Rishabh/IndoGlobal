25cae9989da8e15314008eaa3a582131cc3b4077
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-11-26 12:09:29 +0530

    Fixed Job Application form

--- src/apps/careers/Careers.jsx
+++ src/apps/careers/Careers.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Container from '../../components/wrappers/Container'
+
 import JobForm from './JobForm'
 
 const Careers = () => {
   return (
-    <section className='p-0 py-10 bg-slate-100'>
+    <section className='p-0 sm:p-10 bg-slate-100'>
         <JobForm/>
     </section>
   )
